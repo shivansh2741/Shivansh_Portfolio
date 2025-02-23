95e9ea28eb884dea2280880b613fcf8f63f98d99
shivansh2741 <shivanshd10@gmail.com>
2025-02-23 19:16:53 +0530

    removed anchor tag

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,25 +7,22 @@ const ProjectCard = ({ title, description, git, technologies }) => {
     return (
         <div className="h-auto max-w-[400px] sm:max-w-[500px] md:max-w-[600px] lg:max-w-[700px] bg-gray-900 border border-neutral-100 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 p-4 sm:p-6">
             {title === "Trektopia" && (
-                <a>
-                    <img
-                        className="w-full rounded-t-lg object-cover h-60 sm:h-72 md:h-80"
-                        src={Tourism}
-                        alt="Trektopia"
-                    />
-                </a>
+                <img
+                    className="w-full rounded-t-lg object-cover h-60 sm:h-72 md:h-80"
+                    src={Tourism}
+                    alt="Trektopia"
+                />
             )}
             {title === "Hostellaro" && (
-                <a>
-                    <img
-                        className="w-full rounded-t-lg object-cover h-60 sm:h-72 md:h-80"
-                        src={Hostellaro}
-                        alt="Hostellaro"
-                    />
-                </a>
+                <img
+                    className="w-full rounded-t-lg object-cover h-60 sm:h-72 md:h-80"
+                    src={Hostellaro}
+                    alt="Hostellaro"
+                />
+
             )}
             <div className="p-4">
-                <a>
+                <a href={git} target="_blank" rel="noopener noreferrer">
                     <h5 className="text-xl sm:text-2xl md:text-3xl font-bold tracking-tight text-white bg-clip-text text-transparent bg-gradient-to-r from-yellow-200 to-pink-500">
                         {title}
                     </h5>
@@ -49,14 +46,14 @@ const ProjectCard = ({ title, description, git, technologies }) => {
 
                 <a
                     href={git}
-                    rel="noreferrer"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="mt-4 px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 text-sm sm:text-md md:text-lg font-semibold text-white bg-gray-800 border border-gray-700 rounded-lg shadow-lg hover:bg-gray-700 hover:text-green-400 transition duration-300"
                 >
                     GitHub
                 </a>
             </div>
         </div>
-
     );
 };
 
@@ -67,10 +64,8 @@ const Projects = () => {
                 {project.map((item, index) => (
                     <ProjectCard
                         key={index}
-                        image={item.image}
                         title={item.title}
                         description={item.description}
-                        links={item.links}
                         git={item.git}
                         technologies={item.technologies}
                     />
@@ -86,7 +81,6 @@ export const project = [
         title: "Hostellaro",
         description:
             "A full-stack hostel management system designed for students, wardens, and admins, featuring automated room allocation, issue tracking, and real-time communication.",
-        image: { Tourism },
         git: "https://github.com/shivansh2741/Dorm_Management",
         technologies: ["Flutter", "Dart", "Firebase"],
     },
@@ -94,11 +88,9 @@ export const project = [
         title: "Trektopia",
         description:
             "A backend system for managing tour packages, implementing role-based access control, secure user management, and an optimized booking experience.",
-        image: { Hostellaro },
         git: "https://github.com/shivansh2741/TrekTopia",
         technologies: ["HTML", "CSS", "EJS", "NodeJS", "ExpressJS", "MySQL"],
     },
 ];
 
 export default Projects;
-
